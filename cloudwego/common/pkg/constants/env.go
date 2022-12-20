cb0a294106b0d0242ace6a97ab8c93db68a2ea23
weedge <weege007@gmail.com>
2022-12-20 18:36:46 +0800

    add common pkg/constants

---
+++ cloudwego/common/pkg/constants/env.go
@@ -0,0 +1,12 @@
+package constants
+
+const (
+	RunEnv    = "RUN_ENV"
+	EnvLocal  = "local"
+	EnvDev    = "dev"
+	EnvTest   = "test"
+	EnvPre    = "pre"
+	EnvStress = "stress"
+	EnvGray   = "gray"
+	EnvProd   = "prod"
+)
---
+++ cloudwego/common/pkg/constants/log_meta_key_enum.go
@@ -0,0 +1,63 @@
+package constants
+
+// biz meta key
+const (
+	ProjectNameKey = "projectName"
+	AppNameKey     = "appName"
+	EnvKey         = "env"
+	LocalIpKey     = "localIp"
+	OutBoundIpKey  = "outBoundIp"
+)
+
+// http header key
+const (
+	SessionId            = "Session-Id"
+	RequestSessionId     = "Request-Session-Id"
+	LastRequestSessionId = "Last-Request-Session-Id"
+	XRequestId           = "X-Request-ID"
+	CurrentUserId        = "Current-User-Id"
+
+	XB3ZipKinOpenTraceId = "x-b3-traceid"       // open tracing id for B3 ZipKin https://github.com/openzipkin/b3-propagation
+	XRequestRunPath      = "x-request-run-path" // for coreDNS https://{git branch}.develop.com, default master branch as base
+	TraceId              = "trace-id"           //the same to x-request-id  as trace log id
+)
+
+// mq property key
+const (
+	MqTraceIdKey = "mq_trace_id"
+)
+
+// log type
+const (
+	LogTypeKey        = "LOGTYPE"
+	LogTypeServer     = "server"   // biz server logic log
+	LogTypeAccess     = "access"   // access log
+	LogTypeMqConsumer = "consumer" // mq consumer
+	LogTypeModule     = "module"   // dependent resource module (db,cache)
+	LogTypeHttpReq    = "httpreq"  // dependent http resource access lay request/response
+	LogTypeJob        = "job"      // cron job
+)
+
+// log type -> key  related lib to define
+const ()
+
+// access resource proto
+const (
+	ProtoKey       = "PROTO"
+	ProtoKeyHttp   = "http"
+	ProtoKeyGRPC   = "grpc"
+	ProtoKeyThrift = "thrift"
+	ProtoKeyDubbo  = "dubbo"
+	ProtoKeyRPCX   = "rpcx"
+	ProtoKeyRedis  = "redis"
+	ProtoKeyMongo  = "mongo"
+	ProtoKeyPgSql  = "pgsql"
+	ProtoKeyMySql  = "mysql"
+)
+
+// cost
+const (
+	CostKey             = "cost"
+	RequestStartTimeKey = "requestStartTime"
+	RequestEndTimeKey   = "requestEndTime"
+)
