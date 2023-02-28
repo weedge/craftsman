6a28553051bc08f9576d7e74d32b3febb1f41fb3
weedge <weege007@gmail.com>
2023-03-01 00:36:28 +0800

    python lambda handler for login logic

---
+++ cloud/aws/cdk/serverless-openai-chatbot/server/lambda/python/login/login.py
@@ -0,0 +1,71 @@
+"""
+人生苦短
+
+help doc:
+https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dynamodb.html
+"""
+
+import logging
+import boto3
+import json
+import jwt
+import os
+from datetime import datetime, timedelta
+
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+dynamodbClient = boto3.client("dynamodb", region_name="us-east-1")
+TABLE_NAME = "chat_user_info"
+
+
+def create_token(user_name):
+    return jwt.encode(payload={
+        "username": user_name,
+        "exp": datetime.utcnow() + timedelta(days=1)
+    }, key=os.getenv("TOKEN_KEY"))
+
+
+def format_response(code, err_msg, token):
+    return {
+        "isAuthorized": code == 200,
+        "body": {
+            "message": err_msg,
+            "token": token,
+        }
+    }
+
+
+def query_dynamodb(key):
+    try:
+        res = dynamodbClient.get_item(
+            TableName=TABLE_NAME,
+            Key={'username': {'S': key}}
+        )
+        logger.info("res", res)
+        if res.Item:
+            return res.Item.password.S
+    except Exception as e:
+        logger.error("except %s", e)
+        return None
+
+    return None
+
+
+def handler(event, context):
+    logger.info("Request: %s", event)
+    response_code = 200
+
+    body = json.loads(event.get('body'))
+    logger.info("body: %s", body)
+    pwd = query_dynamodb(body.username)
+    if pwd is None:
+        return format_response(403, "user not found", "")
+
+    if pwd != body.password:
+        return format_response(403, "Invalid password, please check it", "")
+
+    token = create_token(body.username)
+    logger.info("create token: %s", token)
+    return format_response(200, "success", token)
---
+++ cloud/aws/cdk/serverless-openai-chatbot/server/lambda/python/readme.md
@@ -0,0 +1,20 @@
+### Develop
+```shell
+mkdir <new lambda python handler func name> && cd {name}
+python3 -m venv .venv
+source .venv/bin/activate
+pip install boto3
+pip install pyjwt
+pip freeze > requirements.txt
+```
+
+### Install
+```shell
+git clone github.com/weedge/craftsman 
+cd craftsman/cloud/aws/cdk/serverless-openai-chatbot/server/lambda/python/login
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+```
+
+
