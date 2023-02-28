a3944c9b958a87bb5fee54ae26ed107459085d9c
weedge <weege007@gmail.com>
2023-03-01 00:33:58 +0800

    golang lambda handler for push logic

---
+++ cloud/aws/cdk/serverless-openai-chatbot/server/lambda/golang/push/api/openai/text_completion.go
@@ -0,0 +1,79 @@
+package openai
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"strings"
+
+	gogpt "github.com/sashabaranov/go-gpt3"
+)
+
+const (
+	MaxTokens   = 2000
+	Temperature = 0.7
+)
+
+var textModels = map[string]struct{}{
+	gogpt.GPT3TextDavinci003:      {},
+	gogpt.GPT3TextDavinci002:      {},
+	gogpt.GPT3TextCurie001:        {},
+	gogpt.GPT3TextBabbage001:      {},
+	gogpt.GPT3TextAda001:          {},
+	gogpt.GPT3TextDavinci001:      {},
+	gogpt.GPT3DavinciInstructBeta: {},
+	gogpt.GPT3Davinci:             {},
+	gogpt.GPT3CurieInstructBeta:   {},
+	gogpt.GPT3Curie:               {},
+	gogpt.GPT3Ada:                 {},
+	gogpt.GPT3Babbage:             {},
+}
+
+func GetTextCompletion(ctx context.Context, client *gogpt.Client, req gogpt.CompletionRequest) (res string, err error) {
+	if req.Stream {
+		return
+	}
+
+	resp, err := client.CreateCompletion(ctx, req)
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+
+	return strings.TrimPrefix(resp.Choices[0].Text, "\n\n"), nil
+}
+
+func GetTextCompletionStream(ctx context.Context, client *gogpt.Client, req gogpt.CompletionRequest) (string, error) {
+	if !req.Stream {
+		return "", nil
+	}
+
+	stream, err := client.CreateCompletionStream(ctx, req)
+	if err != nil {
+		fmt.Println("Stream err", err.Error())
+		return "", err
+	}
+	defer stream.Close()
+
+	index := 0
+	res := strings.Builder{}
+	for {
+		response, err := stream.Recv()
+		if errors.Is(err, io.EOF) {
+			break
+		}
+		if err != nil {
+			log.Printf("Stream error:%s\n", err.Error())
+			return "", err
+		}
+
+		if index > 1 {
+			word := response.Choices[0].Text
+			res.WriteString(word)
+		}
+		index++
+	}
+	return res.String(), nil
+}
---
+++ cloud/aws/cdk/serverless-openai-chatbot/server/lambda/golang/push/main.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"net/url"
+	"os"
+	"strings"
+	"unsafe"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	apigw "github.com/aws/aws-sdk-go-v2/service/apigatewaymanagementapi"
+	gogpt "github.com/sashabaranov/go-gpt3"
+	"github.com/weedge/craftsman/cloud/aws/cdk/serverless-openai-chatbot/server/lambda/golang/push/api/openai"
+)
+
+var gogptClient *gogpt.Client
+var apiWsGwClient *apigw.Client
+
+func Init() {
+	gogptClient = gogpt.NewClient(os.Getenv("OPENAI_API_KEY"))
+
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("us-east-1"))
+	if err != nil {
+		log.Fatalf("unable to load SDK config, %v", err)
+	}
+	apiWsGwClient = apigw.NewFromConfig(cfg)
+}
+
+func String(b []byte) string {
+	return *(*string)(unsafe.Pointer(&b))
+}
+func Str2Bytes(s string) []byte {
+	x := (*[2]uintptr)(unsafe.Pointer(&s))
+	h := [3]uintptr{x[0], x[1], x[1]}
+	return *(*[]byte)(unsafe.Pointer(&h))
+}
+
+type WsEventMsg struct {
+	Action  string  `json:"action"`
+	Payload Payload `json:"payload"`
+}
+type Payload struct {
+	Msgid  string `json:"msgid"`
+	Prompt string `json:"prompt"`
+	Params Params `json:"params"`
+}
+type Params struct {
+	FrequencyPenalty float32 `json:"frequency_penalty"`
+	MaxTokens        int     `json:"max_tokens"`
+	PresencePenalty  float32 `json:"presence_penalty"`
+	Temperature      float32 `json:"temperature"`
+	TopP             float32 `json:"top_p"`
+	ModelName        string  `json:"model_name"`
+}
+type Message struct {
+	RequestContext events.APIGatewayWebsocketProxyRequestContext
+	Payload        Payload
+}
+
+func Handler(ctx context.Context, snsEvent events.SNSEvent) (err error) {
+	for _, record := range snsEvent.Records {
+		snsRecord := record.SNS
+		log.Printf("[%s %s] Message = %s \n", record.EventSource, snsRecord.Timestamp, snsRecord.Message)
+		msg := &Message{}
+		err = json.Unmarshal(Str2Bytes(snsRecord.Message), msg)
+		if err != nil {
+			log.Printf("error:%s\n", err.Error())
+			return
+		}
+		prompt := strings.Trim(msg.Payload.Prompt, " ")
+		if prompt == "" {
+			log.Println("empty prompt")
+			return
+		}
+
+		params := msg.Payload.Params
+		res := ""
+		res, err = openai.GetTextCompletionStream(ctx, gogptClient, gogpt.CompletionRequest{
+			Model:            params.ModelName,
+			Prompt:           prompt,
+			MaxTokens:        params.MaxTokens,
+			Temperature:      float32(params.Temperature),
+			TopP:             float32(params.TopP),
+			Stream:           true,
+			PresencePenalty:  float32(params.PresencePenalty),
+			FrequencyPenalty: params.FrequencyPenalty,
+		})
+		if err != nil {
+			res = err.Error()
+		}
+
+		reqCtx := msg.RequestContext
+		postData, _ := json.Marshal(map[string]any{"msgid": reqCtx.MessageID, "text": res})
+		apiGwUrl := &url.URL{
+			Scheme: "https",
+			Host:   reqCtx.DomainName,
+			Path:   reqCtx.Stage,
+		}
+		postRes, err1 := apiWsGwClient.PostToConnection(ctx, &apigw.PostToConnectionInput{
+			ConnectionId: aws.String(reqCtx.ConnectionID),
+			Data:         postData,
+		}, func(o *apigw.Options) {
+			o.EndpointResolver = apigw.EndpointResolverFromURL(apiGwUrl.String())
+		})
+		if err1 != nil {
+			log.Printf("post err:%s\n", err1.Error())
+			err = err1
+			return
+		}
+		log.Printf("post res:%+v\n", *postRes)
+	}
+
+	return
+}
+
+func main() {
+	Init()
+	lambda.Start(Handler)
+}
